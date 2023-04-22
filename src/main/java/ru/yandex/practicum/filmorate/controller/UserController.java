158a992dd83e5992d6b54fd355a1a71af0038b4f
Anna TS <ann_0791@mail.ru>
2023-04-22 19:56:50 +0300

    Update .gitignore

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,16 +1,13 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.controller.validators.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -19,9 +16,9 @@ import java.util.stream.Collectors;
 @RestController
 public class UserController {
 
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final HashMap<Integer, User> users = new HashMap<>();
-    private int idU =1;
+    private int idU = 1;
 
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
@@ -52,10 +49,7 @@ public class UserController {
     @GetMapping("/users")
     public List<User> findAll() {
 
-        return users.entrySet()
-                .stream()
-                .map(e -> e.getValue())
-                .collect(Collectors.toList());
+        return users.entrySet().stream().map(e -> e.getValue()).collect(Collectors.toList());
     }
 
     @PutMapping("/users")
